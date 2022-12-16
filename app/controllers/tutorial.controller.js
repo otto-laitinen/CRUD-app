c395752c1630e3c6139616e56ac98f8700a96558
Otto <ottolaitinen@outlook.com>
2022-12-16 16:14:44 +0200

    connection to db

---
+++ app/controllers/tutorial.controller.js
@@ -0,0 +1,38 @@
+const db = require("../models");
+const Tutorial = db.tutorials;
+const Op = db.Sequelize.Op; // Op object properties are used to create SQL queries with operators
+
+// CREATE and SAVE a new tutorial
+exports.create = (req, res) => {
+
+};
+
+// FIND ALL tutorials
+exports.findAll = (req, res) => {
+
+};
+
+// FIND a tutorial by id
+exports.findOne = (req, res) => {
+
+};
+
+// UPDATE a tutorial by the id in the request
+exports.update = (req, res) => {
+
+};
+
+// DELETE a tutorial by the id in the request
+exports.delete = (req, res) => {
+
+};
+
+// DELETE ALL tutorials from db
+exports.deleteAll = (req, res) => {
+
+};
+
+// Find all published tutorials
+exports.findAllPublished = (req, res) => {
+
+};
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');               // provides Express middleware to en
 
 const app = express();
 
+
 var corsOptions = {origin: "http://localhost:8081"};
 app.use(cors(corsOptions));
 
@@ -13,6 +14,14 @@ app.use(bodyParser.json());
 // parse requests of content-type 'application/x-www-form-urlencoded'
 // extended: true precises that the req.body object will contain values of any type instead of just strings
 app.use(bodyParser.urlencoded({ extended: true}));
+
+// db
+const db = require("./app/models");
+db.sequelize.sync();
+// In development, you may need to drop existing tables and re-sync database:
+// db.sequelize.sync({ force: true }).then(() => {
+//     console.log("Drop and re-sync db.");
+//   });
 
 // simple route
 app.get('/', (req, res) => {
